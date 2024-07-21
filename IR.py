ad6e80c93e331fe6f89ffd6c43bc43dab8cd087f
yangssssuo <100421385+yangssssuo@users.noreply.github.com>
2024-07-21 17:32:21 +0800

    Add files via upload

---
+++ IR.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+from model.DataSet import CO_Set
+from model.BertConfigConv import BertConfig
+from model.CO_Bert import BertForPretrain
+from torch.utils.data import Dataset, DataLoader,Subset
+from model.Trainer import ModelTrainer
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = '4'
+def get_parameter_number(net):
+    total_num = sum(p.numel() for p in net.parameters())
+    trainable_num = sum(p.numel() for p in net.parameters() if p.requires_grad)
+    return {'Total': total_num, 'Trainable': trainable_num}
+
+mode = 'IR'
+
+json_file = f'/home/yanggk/CO_Bert/config/config_conv_{mode}.json'
+config = BertConfig.from_json_file(json_file)
+model = BertForPretrain(config)
+print(get_parameter_number(model))
+
+test_set = CO_Set('/home/yanggk/Data/CO_Bert/washed2.txt',mode=mode)
+
+bsz = 256
+
+train_idx = np.load('/home/yanggk/CO_Bert/data/train.npy')
+val_idx = np.load('/home/yanggk/CO_Bert/data/valid.npy')
+test_idx = np.load('/home/yanggk/CO_Bert/data/test.npy')
+
+train_dataset = Subset(test_set,train_idx)
+validate_dataset = Subset(test_set,val_idx)
+test_dataset = Subset(test_set,test_idx)
+print(f'======train size:{len(train_dataset)}======')
+print(f'======valid size:{len(validate_dataset)}======')
+print(f'======test size:{len(test_dataset)}======')
+
+# train_dataset, validate_dataset, test_dataset = torch.utils.data.random_split(test_set, [train_size, val_size, test_size])
+
+train_loader = DataLoader(train_dataset, batch_size=bsz, shuffle=True, num_workers=8,pin_memory=True)
+validate_loader = DataLoader(validate_dataset, batch_size=bsz, shuffle=True, num_workers=8,pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True, num_workers=4)
+
+trainer = ModelTrainer(model,mode)
+trainer.start_training(train_dataloader=train_loader,
+                       valid_dataloader=validate_loader,
+                       test_loader=test_loader,
+                       epochs=1000,
+                       continue_train=False)
+
+# test_loss,draw_datas,Label_data = trainer.test(test_loader)
+# print(f'Test Loss: {test_loss:.4f}')
+#         # print(draw_datas)
+# trainer.draw_pict(draw_datas)
+# trainer.draw_hm(Label_data)
---
+++ masked_fusion.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+from model.MaskedFusionSet import FusionSet
+from model.MaskedFusionBert import FusionBert
+from torch.utils.data import Dataset, DataLoader,Subset
+from model.Trainer_fusion_masked import ModelTrainer
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+def get_parameter_number(net):
+    total_num = sum(p.numel() for p in net.parameters())
+    trainable_num = sum(p.numel() for p in net.parameters() if p.requires_grad)
+    return {'Total': total_num, 'Trainable': trainable_num}
+
+
+mode = ['IR','Raman','Coord']
+mask_lis=[1,1,1]
+model = FusionBert(mode[0],mode[1],mode[2])
+print(get_parameter_number(model))
+# print(model)
+# print(model.parameters())
+test_set = FusionSet('/home/yanggk/Data/CO_Bert/washed2.txt')
+
+bsz = 256
+
+train_idx = np.load('data/train.npy')
+val_idx = np.load('data/valid.npy')
+test_idx = np.load('data/test.npy')
+
+train_dataset = Subset(test_set,train_idx)
+validate_dataset = Subset(test_set,val_idx)
+test_dataset = Subset(test_set,test_idx)
+print(f'======train size:{len(train_dataset)}======')
+print(f'======valid size:{len(validate_dataset)}======')
+print(f'======test size:{len(test_dataset)}======')
+
+train_loader = DataLoader(train_dataset, batch_size=bsz, shuffle=True, num_workers=16,pin_memory=True)
+validate_loader = DataLoader(validate_dataset, batch_size=bsz, shuffle=True, num_workers=16,pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True, num_workers=8)
+
+trainer = ModelTrainer(model,
+                       mode=mode,
+                       mask_lis=mask_lis)
+trainer.start_training(train_dataloader=train_loader,
+                       valid_dataloader=validate_loader,
+                       test_loader=test_loader, 
+                       epochs=500,
+                       continue_train=False)
+
